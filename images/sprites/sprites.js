d34f76978fb910d389ec34157f8f42037b0ff358
davidjhughes <hughesdavidj@gmail.com>
2018-01-15 20:01:33 -0700

    More Texture Atlas

---
+++ images/sprites/sprites.js
@@ -0,0 +1,30 @@
+{
+"frames": {
+	"blob.png": {
+		"frame": {"x":0, "y":514, "w":32, "h":24},
+		"spriteSourceSize": {"x":0,"y":0,"w":32,"h":24},
+		"sourceSize": {"w":32,"h":24}
+	},
+	"dungeon.png": {
+		"frame": {"x":0, "y":0, "w":512, "h":512},
+		"spriteSourceSize": {"x":0,"y":0,"w":512,"h":512},
+		"sourceSize": {"w":512,"h":512}
+	},
+	"explorer.png": {
+		"frame": {"x":0, "y":566, "w":20, "h":32},
+		"spriteSourceSize": {"x":0,"y":0,"w":21,"h":32},
+		"sourceSize": {"w":21,"h":32}
+	},
+	"treasure.png": {
+		"frame": {"x":0, "y":540, "w":28, "h":24},
+		"spriteSourceSize": {"x":0,"y":0,"w":28,"h":24},
+		"sourceSize": {"w":28,"h":24}
+	}
+
+},
+"meta": {
+	"image": "sprites.png",
+	"size": {"w": 1024, "h": 1024},
+	"scale": "1"
+}
+}
--- scripts/app.js
+++ scripts/app.js
@@ -29,8 +29,11 @@ document.body.appendChild(app.view);
 
 loader
 .add(["../images/monkeyface.jpg", "../images/09.png"])
+.add("../images/sprites/sprites.json")
 .on("progress", loadProgressHandler)
 .load(setup);
+
+let dungeon, explorer, treasure, blob, id;
 
 function setup(){
     // let monkey = new Sprite(resources["../images/monkeyface.jpg"].texture);
@@ -39,15 +42,69 @@ function setup(){
     // monkey.anchor.set(0.5,0.5);
     // monkey.position.set(monkey.width, monkey.height);
     // monkey.rotation = 0.6;
-    let tileset = TextureCache["../images/09.png"];
-    let rect = new Rectangle(192, 128, 64, 64);
-    tileset.frame = rect;
-    let rocket = new Sprite(tileset);
-    rocket.position.set(32,32);
-    app.stage.addChild(rocket);
+    // let tileset = TextureCache["../images/09.png"];
+    // let rect = new Rectangle(192, 128, 64, 64);
+    // tileset.frame = rect;
+    // let rocket = new Sprite(tileset);
+    // rocket.position.set(32,32);
+    // app.stage.addChild(rocket);
     // app.renderer.render(stage);
-    console.log("All Files Loaded.");
+
+    id = resources["../images/sprites/sprites.json"].textures;
+    dungeon = new Sprite(id["dungeon.png"]);
+    app.stage.addChild(dungeon);
+    explorer = new Sprite(id["explorer.png"]);
+    app.stage.addChild(explorer);
+    treasure = new Sprite(id["treasure.png"]);
+    app.stage.addChild(treasure);
+
+    
+    
+    treasure.position.set(
+        app.stage.width  - treasure.width - 48,
+        app.stage.height / 2 - treasure.height / 2
+    )
+    
+    explorer.position.set(
+        68,
+        app.stage.height / 2 - explorer.height / 2
+    )
+
+
+    //Make the blobs
+  let numberOfBlobs = 6,
+  spacing = 48,
+  xOffset = 150;
+
+    //Make as many blobs as there are `numberOfBlobs`
+    for (let i = 0; i < numberOfBlobs; i++) {
+
+        //Make a blob
+        let blob = new Sprite(id["blob.png"]);
+
+        //Space each blob horizontally according to the `spacing` value.
+        //`xOffset` determines the point from the left of the screen
+        //at which the first blob should be added.
+        let x = spacing * i + xOffset;
+
+        //Give the blob a random y position
+        //(`randomInt` is a custom function - see below)
+        let y = randomInt(0, app.stage.height - blob.height);
+
+        //Set the blob's position
+        blob.x = x;
+        blob.y = y;
+
+        //Add the blob sprite to the stage
+        app.stage.addChild(blob);
+    }
 }
+
+//The `randomInt` helper function
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
 
 function loadProgressHandler(loader, resource){
     console.log("loading: " + resource.url);
